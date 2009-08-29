ae719952731c0b724bf583903a55d6fbf065cf11
John Manoogian III <jm3@jm3.net>
2009-08-28 23:16:02 -0700

    home SDL won't play the bells wav; switching back

--- app_racket.rb
+++ app_racket.rb
@@ -68,7 +68,7 @@ def handle_events
     Event.new('Log In', 
       'A user just logged in',
       Reaction.new( 
-        'sounds/phasing-bendy-bells.wav',
+        'sounds/savage.wav',
         {:color => :yellow, :duration => 4}))
 
   # keep running so long as sounds are playing
